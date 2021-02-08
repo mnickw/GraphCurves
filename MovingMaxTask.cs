b56f0b64f36ece2cc56e84f8c9b008a6be1d31ee
Nikita Minyuk <nikita.minyuk.w@gmail.com>
2021-02-08 22:02:28 +0300

    Add MovingMaxTask class

--- MovingMaxTask.cs
+++ MovingMaxTask.cs
@@ -4,13 +4,30 @@ using System.Linq;
 
 namespace yield
 {
-
 	public static class MovingMaxTask
 	{
 		public static IEnumerable<DataPoint> MovingMax(this IEnumerable<DataPoint> data, int windowWidth)
 		{
-			//Fix me!
-			return data;
+			if (windowWidth <= 0)
+				throw new System.ArgumentOutOfRangeException();
+			int i = 1;
+			LinkedList<double> maxPotentials = new LinkedList<double>();
+			Queue<double> windowNumbers = new Queue<double>();
+			foreach (DataPoint dataPoint in data)
+			{
+				if (i <= windowWidth)
+					i++;
+				else if (maxPotentials.Count == 0)
+					windowNumbers.Dequeue();
+				else if (maxPotentials.First.Value == windowNumbers.Dequeue())
+					maxPotentials.RemoveFirst();
+				windowNumbers.Enqueue(dataPoint.OriginalY);
+				while (maxPotentials.Count > 0 && maxPotentials.Last.Value <= dataPoint.OriginalY)
+					maxPotentials.RemoveLast();
+				maxPotentials.AddLast(dataPoint.OriginalY);
+				var newDataPoint = dataPoint.WithMaxY(maxPotentials.First.Value);
+				yield return newDataPoint;
+			}
 		}
 	}
 }

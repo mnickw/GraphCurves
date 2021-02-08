5303962a7af057929e155caf8ced965173ce428a
Nikita Minyuk <nikita.minyuk.w@gmail.com>
2021-02-08 21:59:50 +0300

    Add MovingAverageTask class

--- MovingAverageTask.cs
+++ MovingAverageTask.cs
@@ -6,8 +6,24 @@ namespace yield
 	{
 		public static IEnumerable<DataPoint> MovingAverage(this IEnumerable<DataPoint> data, int windowWidth)
 		{
-			//Fix me!
-			return data;
+			if (windowWidth <= 0)
+				throw new System.ArgumentOutOfRangeException();
+			Queue<double> lastYs = new Queue<double>();
+			double sum = 0;
+			double result = 0;
+			foreach (var dataPoint in data)
+			{
+				if (lastYs.Count < windowWidth)
+				{
+					sum += dataPoint.OriginalY;
+					result = sum / (lastYs.Count + 1);
+				}
+				else
+					result += (dataPoint.OriginalY - lastYs.Dequeue()) / windowWidth;
+				var newDataPoint = dataPoint.WithAvgSmoothedY(result);
+				yield return newDataPoint;
+				lastYs.Enqueue(dataPoint.OriginalY);
+			}
 		}
 	}
 }
